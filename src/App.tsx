d440265e0d2f368b6f32327eec1e37eba038a966
Mariam Lagazauri <114602082+mariamlag@users.noreply.github.com>
2023-06-05 12:29:17 +0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
       <GlobalStyles />
         <Helmet>
           <link rel="preconnect" href="https://fonts.googleapis.com"/>
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"/>
           <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap" rel="stylesheet"/>
         </Helmet>
       <div>
